4eddf3e050510580e6b5614cf3a09f722f64ed47
Max de Vocht <maxdevocht@hotmail.com>
2024-01-03 14:35:22 +0100

    Added index.html
    Added index.html

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstCard = 2
+let secondCard = 8
+let sum = firstCard + secondCard
+let hasBlackJack = false
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card? 🙂")
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳")
+    hasBlackJack = true
+} else {
+    console.log("Too bad.. You just lost your money! 😭")
+}
