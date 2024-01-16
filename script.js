9007de8cc84b50da6891e0c8e80b814a4c04ed8c
Max de Vocht <maxdevocht@hotmail.com>
2024-01-16 12:27:40 +0100

    Update
    Updated the game. Next going to add buttons

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-let firstCard = 2
-let secondCard = 8
+let firstCard = 10
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-let message  ""
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "Too bad.. You just lost your money! 😭"
-    isAlive = false
+function startGame() {
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "Too bad.. You just lost your money!"
+        isAlive = false
+    }
+    messageEl.textContent = message   
 }
-
-console.log(message)
